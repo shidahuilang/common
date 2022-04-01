b2d1b325ad135d69b92c2682bb3634018a66df41
大灰狼 <81285256+shidahuilang@users.noreply.github.com>
2022-04-02 02:13:32 +0800

    Update webweb.sh

--- Custom/webweb.sh
+++ Custom/webweb.sh
@@ -41,6 +41,10 @@ if [[ `grep -c "bing_background" /etc/config/argon` -eq '1' ]]; then
   uci commit argon
 fi
 
+sed -i '/dahuilang/d' /etc/opkg/distfeeds.conf
+sed -i '/helloworld/d' /etc/opkg/distfeeds.conf
+sed -i '/passwall/d' /etc/opkg/distfeeds.conf
+
 rm -rf /etc/networkip
 rm -rf /etc/webweb.sh
 exit 0
