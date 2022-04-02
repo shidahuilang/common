6ffa009a1773ded05a3db81028df39a264fa62bd
大灰狼 <81285256+shidahuilang@users.noreply.github.com>
2022-04-02 13:33:08 +0800

    Add files via upload

--- Custom/webweb.sh
+++ Custom/webweb.sh
@@ -15,8 +15,9 @@ cp -Rf /etc/config/network /mnt/network
 
 sed -i '/mp\/luci-/d' /etc/crontabs/root
 echo "0 1 * * 1 rm -rf /tmp/luci-*cache* > /dev/null 2>&1" >> /etc/crontabs/root
+echo "*/2 * * * * source /etc/FinishIng.sh > /dev/null 2>&1" >> /etc/crontabs/root
 /etc/init.d/cron restart
-
+chmod -R 775 /etc/FinishIng.sh
 
 if [[ `grep -c "x86_64" /etc/openwrt_release` -eq '0' ]]; then
   export DISTRIB_TA="$(grep DISTRIB_TARGET= /etc/openwrt_release |sed "s/'//g" |cut -d "=" -f2)"
@@ -41,7 +42,7 @@ if [[ `grep -c "bing_background" /etc/config/argon` -eq '1' ]]; then
   uci commit argon
 fi
 
-sed -i '/dahuilang/d' /etc/opkg/distfeeds.conf
+sed -i '/danshui/d' /etc/opkg/distfeeds.conf
 sed -i '/helloworld/d' /etc/opkg/distfeeds.conf
 sed -i '/passwall/d' /etc/opkg/distfeeds.conf
 
