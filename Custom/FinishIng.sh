ce363db2d0825808895843e37e37583f75143219
大灰狼 <81285256+shidahuilang@users.noreply.github.com>
2022-04-03 11:37:53 +0800

    Update FinishIng.sh

--- Custom/FinishIng.sh
+++ Custom/FinishIng.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+SSS="package/lean/default-settings/files/zzz-default-settings"
 sed -i '/coremark.sh/d' feeds/packages/utils/coremark/coremark
-cat >> $ZZZ <<'EOF'
+cat >> $SSS <<'EOF'
 cat /dev/null > /etc/bench.log
 echo " (CpuMark : 56983.857988" >> /etc/bench.log
 echo " Scores)" >> /etc/bench.log
 EOF
-sed -i '/exit 0/d' $ZZZ && echo "exit 0" >> $ZZZ
+sed -i '/exit 0/d' $ZZZ && echo "exit 0" >> $SSS
