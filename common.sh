4cee2fbfb6b123acc8bb0616955f24d7f3eacdeb
大灰狼 <81285256+shidahuilang@users.noreply.github.com>
2022-06-17 20:58:27 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -160,23 +160,7 @@ if [[ "${matrixtarget}" == "Lede_source" ]]; then
   fi
   export SOURCE="Lede"
   export LUCI_EDITION="18.06"
-elif [[ "${matrixtarget}" == "Lede_nanopi_r2s" ]]; then
-  export ZZZ_PATH="${HOME_PATH}/package/default-settings/files/zzz-default-settings"
-  if [[ ! -f "${ZZZ_PATH}" ]]; then
-    TIME r "上游源码作者修改了zzz-default-settings文件的路径或者名称，找编译脚本的作者及时修改"
-    exit 1
-  fi
-  export SOURCE="Lede_nanopi_r2s"
-  export LUCI_EDITION="18.06"
-
-elif [[ "${matrixtarget}" == "Lede_nanopi_r4s" ]]; then
-  export ZZZ_PATH="${HOME_PATH}/package/default-settings/files/zzz-default-settings"
-  if [[ ! -f "${ZZZ_PATH}" ]]; then
-    TIME r "上游源码作者修改了zzz-default-settings文件的路径或者名称，找编译脚本的作者及时修改"
-    exit 1
-  fi
-  export SOURCE="Lede_nanopi_r4s"
-  export LUCI_EDITION="18.06"    
+ 
 elif [[ "${matrixtarget}" == "Lienol_source" ]]; then
   export ZZZ_PATH="${HOME_PATH}/package/default-settings/files/zzz-default-settings"
   if [[ ! -f "${ZZZ_PATH}" ]]; then
