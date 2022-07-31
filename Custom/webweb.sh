7b2baa18395cb85a4fec3f6f62cc9e81f2565bdf
大灰狼 <81285256+shidahuilang@users.noreply.github.com>
2022-07-31 23:50:28 +0800

    Update webweb.sh

--- Custom/webweb.sh
+++ Custom/webweb.sh
@@ -4,17 +4,15 @@ touch /etc/crontabs/root
 
 chmod -R 775 /etc/init.d /usr/share
 
-if [[ -f /etc/networkip ]]; then
-  chmod +x /etc/networkip
-  source /etc/networkip
-  uci commit network
-  uci commit dhcp
-  uci commit system
-  uci commit luci
-  uci commit firewall
-  [[ -f /etc/config/ttyd ]] && uci commit ttyd
-  rm -rf /etc/networkip
-fi
+[[ ! -f /mnt/network ]] && chmod +x /etc/networkip && source /etc/networkip
+uci commit network
+uci commit dhcp
+uci commit system
+uci commit luci
+uci commit firewall
+[[ -f /etc/config/ttyd ]] && uci commit ttyd
+rm -rf /etc/networkip
+
 
 if [[ `grep -c "x86_64" /etc/openwrt_release` -eq '0' ]]; then
   export DISTRIB_TA="$(grep DISTRIB_TARGET= /etc/openwrt_release |sed "s/'//g" |cut -d "=" -f2)"
