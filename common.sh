e353275499bb1d45c2efb7786c007f893d064a24
大灰狼 <81285256+shidahuilang@users.noreply.github.com>
2025-01-17 20:28:43 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -1622,28 +1622,63 @@ function Diy_adguardhome() {
   fi
 
 if [[ ! "${weizhicpu}" == "1" ]] && [[ -n "${OpenClash_Core}" ]] && [[ "${OpenClash_branch}" =~ (master|dev) ]]; then
-  echo "正在执行：给openclash下载meta核心"
+  echo "正在执行：给openclash下载核心"
   rm -rf ${HOME_PATH}/files/etc/openclash/core
+  rm -rf ${HOME_PATH}/clash-neihe && mkdir -p ${HOME_PATH}/clash-neihe
   mkdir -p ${HOME_PATH}/files/etc/openclash/core
   cd ${HOME_PATH}/clash-neihe
-
-  # 下载meta内核
-  wget -q https://raw.githubusercontent.com/vernesong/OpenClash/core/${OpenClash_branch}/meta/clash-${Archclash}.tar.gz -O meta.tar.gz
-  
-  # 解压并移动meta内核
-  tar -zxvf meta.tar.gz -O > clash_meta
-  if [[ $? -eq 0 ]]; then
-    mv -f ${HOME_PATH}/clash-neihe/clash_meta ${HOME_PATH}/files/etc/openclash/core/clash_meta
-    sudo chmod +x ${HOME_PATH}/files/etc/openclash/core/clash_meta
-    echo "OpenClash增加meta内核成功"
+  if [[ "${OpenClash_Core}" == "2" ]]; then
+    wget -q https://raw.githubusercontent.com/vernesong/OpenClash/core/${OpenClash_branch}/meta/clash-${Archclash}.tar.gz -O meta.tar.gz
+    wget -q https://raw.githubusercontent.com/vernesong/OpenClash/core/${OpenClash_branch}/dev/clash-${Archclash}.tar.gz -O clash.tar.gz
+    wget -q https://raw.githubusercontent.com/vernesong/OpenClash/core/${OpenClash_branch}/core_version -O core_version
+    TUN="$(cat core_version |grep -v "^v\|^V\|^a" |grep -E "[0-9]+.[0-9]+.[0-9]+")"
+    wget -q https://raw.githubusercontent.com/vernesong/OpenClash/core/${OpenClash_branch}/premium/clash-${Archclash}-${TUN}.gz -O clash_tun.gz
+    
+    tar -zxvf clash.tar.gz -O > clash
+    if [[ $? -eq 0 ]];then
+      mv -f ${HOME_PATH}/clash-neihe/clash ${HOME_PATH}/files/etc/openclash/core/clash
+      sudo chmod +x ${HOME_PATH}/files/etc/openclash/core/clash
+      echo "OpenClash增加dev内核成功"
+    else
+      echo "OpenClash增加dev内核失败"
+    fi
+    tar -zxvf meta.tar.gz -O > clash_meta
+    if [[ $? -eq 0 ]];then
+      mv -f ${HOME_PATH}/clash-neihe/clash_meta ${HOME_PATH}/files/etc/openclash/core/clash_meta
+      sudo chmod +x ${HOME_PATH}/files/etc/openclash/core/clash_meta
+      echo "OpenClash增加meta内核成功"
+    else
+      echo "OpenClash增加meta内核失败"
+    fi
+    gzip -d clash_tun.gz
+    if [[ $? -eq 0 ]];then
+      mv -f ${HOME_PATH}/clash-neihe/clash_tun ${HOME_PATH}/files/etc/openclash/core/clash_tun
+      sudo chmod +x ${HOME_PATH}/files/etc/openclash/core/clash_tun
+      echo "clash"
+      echo "OpenClash增加tun内核成功"
+    else
+      echo "OpenClash增加tun内核失败"
+    fi
+  elif [[ "${OpenClash_Core}" == "1" ]]; then
+    wget -q https://raw.githubusercontent.com/vernesong/OpenClash/core/${OpenClash_branch}/dev/clash-${Archclash}.tar.gz
+    if [[ $? -ne 0 ]];then
+      wget -q https://github.com/vernesong/OpenClash/releases/download/Clash/clash-${Archclash}.tar.gz
+    else
+      echo "OpenClash内核下载成功"
+    fi
+    tar -zxvf clash-${Archclash}.tar.gz
+    if [[ -f "${HOME_PATH}/clash-neihe/clash" ]]; then
+      mv -f ${HOME_PATH}/clash-neihe/clash ${HOME_PATH}/files/etc/openclash/core/clash
+      sudo chmod +x ${HOME_PATH}/files/etc/openclash/core/clash
+      echo "OpenClash增加内核成功"
+    else
+      echo "OpenClash增加内核失败"
+    fi
   else
-    echo "OpenClash增加meta内核失败"
+    echo "无需内核"
   fi
-
   cd ${HOME_PATH}
   rm -rf ${HOME_PATH}/clash-neihe
-else
-  echo "无需内核"
 fi
 
 
